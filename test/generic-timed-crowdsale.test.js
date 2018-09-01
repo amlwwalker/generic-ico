68c5f032a31fa23783727119a2e7e0e33c19fa9b
Alex Walker <amlwwalker@gmail.com>
2018-09-01 12:34:37 +0100

    adding time checks to time based crowdsale

--- test/generic-timed-crowdsale.test.js
+++ test/generic-timed-crowdsale.test.js
@@ -99,14 +99,6 @@ contract("Generic Timed Crowdsale", async function([
       this.crowdsale.sendTransaction({ from: purchaser, value }).should.be
         .fulfilled //rejectedWith(EVMRevert)
     })
-
-    // it("should whitelist an address", async function() {
-    //   const addressToWhitelist = purchaser
-    //   this.crowdsale.addAddressToWhitelist(addressToWhitelist).should.be
-    //     .fulfilled
-    //   const isWhitelisted = await this.crowdsale.whitelist(addressToWhitelist)
-    //   isWhitelisted.should.be.true
-    // })
   })
 
   // describe("test emits", function() {
@@ -120,84 +112,30 @@ contract("Generic Timed Crowdsale", async function([
   //   })
   // })
 
-  // describe("test transaction lifecycle", function() {
-  //   it("should emit events across transaction lifecycle", function(done) {
-  //     this.crowdsale.addAddressToWhitelist(creator)
-  //     const value = web3.utils.toWei(new BN(1), "ether")
+  describe("test transaction lifecycle", function() {
+    it("should emit events across transaction lifecycle", function(done) {
+      this.crowdsale.addAddressToWhitelist(creator)
+      const value = web3.utils.toWei(new BN(1), "ether")
 
-  //     this.crowdsale
-  //       .sendTransaction({ from: creator, value })
-  //       .once("transactionHash", function(hash) {
-  //         hash.should.not.be.null
-  //       })
-  //       .once("receipt", function(receipt) {
-  //         receipt.should.not.be.null
-  //       })
-  //       .on("confirmation", function(confNumber, receipt) {
-  //         confNumber.should.not.be.null
-  //         receipt.should.not.be.null
-  //       })
-  //       .on("error", function(error) {
-  //         error.should.be.null
-  //       })
-  //       .then(function(receipt) {
-  //         receipt.should.not.be.null
-  //         done()
-  //       })
-  //   })
-  // })
-
-  // describe("integration tests", async function() {
-  //   it("should survive a series of calls", async function() {
-  //     const toWhitelist = [payee0, payee1, purchaser, investor]
-  //     const blacklisted = [...addresses]
-
-  //     const value = web3.utils.toWei(new BN(1), "ether")
-
-  //     // someone gets whitelisted
-  //     await this.crowdsale.addAddressToWhitelist(toWhitelist[0])
-
-  //     // someone else tries to send money when not whitelisted
-  //     await this.crowdsale
-  //       .sendTransaction({ from: blacklisted[0], value })
-  //       .should.be.rejectedWith(EVMRevert)
-
-  //     // whitelisted address sends funds
-  //     await this.crowdsale.sendTransaction({ from: toWhitelist[0], value })
-  //       .should.be.fulfilled
-
-  //     // check that tokens were issued
-  //     const whitelistedOneBalance = await this.token.balanceOf(toWhitelist[0])
-  //     await advanceBlock(web3)
-  //     expect(whitelistedOneBalance.eq(value)).to.be.true
-
-  //     // whitelisted several addresses
-  //     await this.crowdsale.addAddressesToWhitelist(toWhitelist.slice(1))
-
-  //     // send funds
-  //     await this.crowdsale.sendTransaction({ from: toWhitelist[1], value })
-  //       .should.be.fulfilled
-  //     await this.crowdsale.sendTransaction({ from: toWhitelist[2], value })
-  //       .should.be.fulfilled
-  //     await this.crowdsale.sendTransaction({ from: toWhitelist[3], value })
-  //       .should.be.fulfilled
-  //     await advanceBlock(web3)
-
-  //     const b1 = await this.token.balanceOf(toWhitelist[1])
-  //     const b2 = await this.token.balanceOf(toWhitelist[2])
-  //     const b3 = await this.token.balanceOf(toWhitelist[3])
-
-  //     expect(b1.eq(value)).to.be.true
-  //     expect(b2.eq(value)).to.be.true
-  //     expect(b3.eq(value)).to.be.true
-
-  //     // remove someone from the whitelist
-  //     this.crowdsale.removeAddressFromWhitelist(toWhitelist[1])
-
-  //     // that address sends funds
-  //     this.crowdsale
-  //       .sendTransaction({ from: toWhitelist[1], value })
-  //       .should.be.rejectedWith(EVMRevert)
-  //   })
-  // })
+      this.crowdsale
+        .sendTransaction({ from: creator, value })
+        .once("transactionHash", function(hash) {
+          hash.should.not.be.null
+        })
+        .once("receipt", function(receipt) {
+          receipt.should.not.be.null
+        })
+        .on("confirmation", function(confNumber, receipt) {
+          confNumber.should.not.be.null
+          receipt.should.not.be.null
+        })
+        .on("error", function(error) {
+          error.should.be.null
+        })
+        .then(function(receipt) {
+          receipt.should.not.be.null
+          done()
+        })
+    })
+  })
 })
